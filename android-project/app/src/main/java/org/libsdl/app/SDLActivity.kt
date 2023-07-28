f9173c8718735e0f8029e50ae17dd203c7fea0b3
huhao1987 <huhao1987@gmail.com>
2023-07-28 15:07:38 +1000

    Allow to setup the c libraries in the Activity

--- android-project/app/src/main/java/org/libsdl/app/SDLActivity.kt
+++ android-project/app/src/main/java/org/libsdl/app/SDLActivity.kt
@@ -85,22 +85,25 @@ open class SDLActivity : AppCompatActivity(), OnSystemUiVisibilityChangeListener
          * It can be overridden by derived classes.
          */
         get() = "SDL_main"
-    protected val libraries: Array<String>
-        /**
-         * This method is called by SDL before loading the native shared libraries.
-         * It can be overridden to provide names of shared libraries to be loaded.
-         * The default implementation returns the defaults. It never returns null.
-         * An array returned by a new implementation must at least contain "SDL2".
-         * Also keep in mind that the order the libraries are loaded may matter.
-         * @return names of shared libraries to be loaded (e.g. "SDL2", "main").
-         */
-        protected get() = arrayOf(
-            "SDL2",  // "SDL2_image",
-            // "SDL2_mixer",
-            // "SDL2_net",
-            // "SDL2_ttf",
-            "main"
-        )
+
+
+    /**
+     * This method is called by SDL before loading the native shared libraries.
+     * It can be overridden to provide names of shared libraries to be loaded.
+     * The default implementation returns the defaults. It never returns null.
+     * An array returned by a new implementation must at least contain "SDL2".
+     * Also keep in mind that the order the libraries are loaded may matter.
+     * @return names of shared libraries to be loaded (e.g. "SDL2", "main").
+     */
+    var libraries = arrayListOf(
+        "SDL2",
+        // "SDL2_image",
+        // "SDL2_mixer",
+        // "SDL2_net",
+        // "SDL2_ttf",
+        "main"
+    )
+
 
     // Load the .so
     fun loadLibraries() {
